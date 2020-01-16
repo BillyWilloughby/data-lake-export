01982b4b1e905a0ab7d6652ba84c81913625cc83
Billy Willoughby <Billy@BillyWilloughby.com>
2020-01-16 09:47:19 -0500

    Adding 7Z Package Generator

---
+++ 7Zip/Create7zFromRelease.bat
@@ -0,0 +1,7 @@
+@echo off
+echo This will create the DataLake Export 7z file for release.
+del DataLakeExport.7z
+7za a -t7z DataLakeExport.7z "..\Data Lake Export\bin\x64\Release\*.dll"
+7za a -t7z DataLakeExport.7z "..\Data Lake Export\bin\x64\Release\*.exe"
+7za a -t7z DataLakeExport.7z "..\Data Lake Export\bin\x64\Release\*.sql"
+7za a -t7z DataLakeExport.7z "..\Data Lake Export\bin\x64\Release\Samples"
