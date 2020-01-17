6dcce1dd514ac5ab1e0aff6ee6cd6afe2d80bf96
Billy Willoughby <Billy@BillyWilloughby.com>
2020-01-17 18:52:30 -0500

    Added / Updated 7z creator for distribution Added Readme and License Started MD version of Read Me, incomplete due to Noteapad ++ issue

--- 7Zip/Create7zFromRelease.bat
+++ 7Zip/Create7zFromRelease.bat
@@ -4,4 +4,6 @@ del DataLakeExport.7z
 7za a -t7z DataLakeExport.7z "..\Data Lake Export\bin\x64\Release\*.dll"
 7za a -t7z DataLakeExport.7z "..\Data Lake Export\bin\x64\Release\*.exe"
 7za a -t7z DataLakeExport.7z "..\Data Lake Export\bin\x64\Release\*.sql"
-7za a -t7z DataLakeExport.7z "..\Data Lake Export\bin\x64\Release\Samples"
+7za a -t7z DataLakeExport.7z "..\Data Lake Export\bin\x64\Release\*.txt"
+7za a -t7z DataLakeExport.7z "..\Data Lake Export\bin\x64\Release\Samples\*.sql"
+7za a -t7z DataLakeExport.7z "..\Data Lake Export\bin\x64\Release\Samples\*.bat"
