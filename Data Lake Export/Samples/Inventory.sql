7ef19d2168035899947443e851be01aec35afc5d
Billy Willoughby <Billy@BillyWilloughby.com>
2020-01-20 19:42:50 -0500

    Code Naming Quality Updates Parse Command Lines Minor Updates to text Major Updates to Export to PDF to fix missing rows Added Quck Table parameter to stop running a query every time during debugging. ***Delete Temporary file to update query results saved***

--- Data Lake Export/Samples/Inventory.sql
+++ Data Lake Export/Samples/Inventory.sql
@@ -1,24 +1,29 @@
 select 
 /*Top doesn't work here, I suppose it's because of the CTE*/
 --CONO, DIVI, WHLO, CompanyLag, DivisionLag, WarehouseLag,
+RowNumber,
 CASE WHEN CompanyLag is null or CompanyLag != CONO then RTRIM(CONO) else '' end Company, 
 CASE WHEN DivisionLag is null or DivisionLag != DIVI then RTRIM(DIVI) else '' end Division, 
 CASE WHEN WarehouseLag is null or WarehouseLag != WHLO then RTRIM(WHLO) else '' end Warehouse, 
-[Item Number],
-[Available Qty],
-[On Order Qty]
+"Item Number",
+"Available Qty",
+"On Order Qty"
+
  from (
 select
-top 10000 --Put TOP HERE
+top 2000 --Put TOP HERE
+ROW_NUMBER ( )   
+    OVER ( Order By CONO, DIVI, WHLO, ITNO)  RowNumber,
 CAST(LAG(MB.CONO,1) OVER (ORDER BY MB.CONO, MB.DIVI, MB.WHLO, MB.ITNO) AS INT) CompanyLag, 
 CAST(LAG(MB.DIVI,1) OVER (ORDER BY MB.CONO, MB.DIVI, MB.WHLO, MB.ITNO) AS CHAR(3)) DivisionLag,
 CAST(LAG(MB.WHLO,1) OVER (ORDER BY MB.CONO, MB.DIVI, MB.WHLO, MB.ITNO) AS VARCHAR(10)) WarehouseLag, 
 MB.CONO, MB.DIVI, MB.WHLO, 
-MB.ITNO [Item Number],
-sum(MB.AVAL) [Available Qty], 
-sum(MB.ORQT) [On Order Qty]
+MB.ITNO "Item Number",
+sum(MB.AVAL) "Available Qty", 
+sum(MB.ORQT) "On Order Qty"
 
 from MITBAL MB
 where MB.deleted = 'false'
 GROUP BY MB.CONO, MB.DIVI, MB.WHLO, MB.ITNO
 ORDER BY MB.CONO, MB.DIVI, MB.WHLO, MB.ITNO) cteTest
+Order By 1
